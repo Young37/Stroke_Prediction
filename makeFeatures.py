304788143a9d19d363454fb5a4db83aafb766306
Young37 <20171470@g.dongseo.ac.kr>
2021-06-11 02:34:09 +0900

    텐서플로에 맞게 데이터 수정코드

--- makeFeatures.py
+++ makeFeatures.py
@@ -4,6 +4,11 @@ import pandas as pd # 판다스 라이브러리 호출
 # pandas의 read_csv()함수로 외부 csv파일을 불러와서 DataFrame으로 저장합니다.
 csv_test = pd.read_csv('C:/Users/skvsn/Desktop/stroke/healthcare-dataset-stroke-data.csv',index_col='id')
 
+# 결측치가 있는 행을 삭제합니다.
+csv_test = csv_test.dropna()
+# 칼럼에 결측치가 있는지 볼 수 있습니다.
+print(csv_test.isnull().sum())
+
 # 행과 열의 개수 파악합니다
 print(csv_test.shape)
 
@@ -34,3 +39,22 @@ print(csv_test)
 
 # 데이터 타입을 출력합니다.
 print(csv_test.dtypes)
+
+# 라벨을 따로 뗍니다.
+stroke = csv_test.pop('stroke')
+stroke
+
+# 피쳐만 따로 뗍니다.
+csv_test.values
+
+import tensorflow as tf
+
+# 자른 조각 합쳐서 텐서플로에서 쓸 수 있도록 하기
+dataset = tf.data.Dataset.from_tensor_slices((csv_test.values, stroke.values))
+
+# 출력 해보기
+for feat, targ in dataset.take(1):
+  print ('Features: {}, Target: {}'.format(feat, targ))
+
+for feat, targ in dataset.take(2):
+  print ('Features: {}, Target: {}'.format(feat, targ))
