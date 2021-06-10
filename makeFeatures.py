941df80e7901cd730e19598e7b717decb7a1bc2f
Young37 <20171470@g.dongseo.ac.kr>
2021-06-11 01:43:37 +0900

    데이터 적절히 가공

--- makeFeatures.py
+++ makeFeatures.py
@@ -10,3 +10,27 @@ print(csv_test.shape)
 # 데이터 전체 출력합니다.
 print(csv_test)
 
+# 데이터 타입을 출력합니다.
+print(csv_test.dtypes)
+
+# 숫자 타입으로 변환합니다.
+csv_test['gender'] = pd.Categorical(csv_test['gender'])
+csv_test['gender'] = csv_test.gender.cat.codes
+
+csv_test['ever_married'] = pd.Categorical(csv_test['ever_married'])
+csv_test['ever_married'] = csv_test.ever_married.cat.codes
+
+csv_test['work_type'] = pd.Categorical(csv_test['work_type'])
+csv_test['work_type'] = csv_test.work_type.cat.codes
+
+csv_test['Residence_type'] = pd.Categorical(csv_test['Residence_type'])
+csv_test['Residence_type'] = csv_test.Residence_type.cat.codes
+
+csv_test['smoking_status'] = pd.Categorical(csv_test['smoking_status'])
+csv_test['smoking_status'] = csv_test.smoking_status.cat.codes
+
+# 데이터 전체 출력합니다.
+print(csv_test)
+
+# 데이터 타입을 출력합니다.
+print(csv_test.dtypes)
