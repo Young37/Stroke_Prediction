7e2d1122e09f282c516473c8f214c288f3713eb8
Young37 <20171470@g.dongseo.ac.kr>
2021-06-10 22:42:57 +0900

    3가지 기법을 적용하기 위한 파이썬 파일들 추가

--- makeFeatures.py
+++ makeFeatures.py
@@ -1,3 +1,10 @@
-import pandas as pd
+import pandas as pd # 판다스 라이브러리 호출
+#행과 열로 이루어진 DataFrame Type데이터를 조작할 때 편한 라이브러리 => pandas
 
-csv_test = pd.read_csv('C:/Users/skvsn/Desktop/stroke/healthcare-dataset-stroke-data')
+# pandas의 read_csv()함수로 외부 csv파일을 불러와서 DataFrame으로 저장합니다.
+csv_test = pd.read_csv('C:/Users/skvsn/Desktop/stroke/healthcare-dataset-stroke-data.csv')
+
+# 행과 열의 개수 파악합니다
+print(csv_test.shape)
+
+print(csv_test)
